f0d9526688a2de7b643802dc12b95df532a9d38d
Judy Joseph <jujoseph@microsoft.com>
2021-08-13 14:08:23 -0700

    Update the SAI 5.0.0.6-1 packages including the DBG package.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -2,7 +2,7 @@ BRCM_SAI = libsaibcm_5.0.0.6-1_amd64.deb
 $(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_5.0.0.6-1_amd64.deb?sv=2015-04-05&sr=b&sig=HA%2FwgMr%2BHnb6zzFCQDfO1WF%2Bf6PBSmIzH13728LTNz4%3D&se=2035-03-31T20%3A45%3A36Z&sp=r"
 BRCM_SAI_DEV = libsaibcm-dev_5.0.0.6-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.6-1_amd64.deb?sv=2015-04-05&sr=b&sig=z634%2BUk14EY5VjEE4tjhvDSP2hiK8s1EJAxjvidl44I%3D&se=2035-03-31T20%3A46%3A17Z&sp=r
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.6-1_amd64.deb?sv=2015-04-05&sr=b&sig=z634%2BUk14EY5VjEE4tjhvDSP2hiK8s1EJAxjvidl44I%3D&se=2035-03-31T20%3A46%3A17Z&sp=r"
 BRCM_SAI_DBG = libsaibcm-dbg_5.0.0.6-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DBG)))
 $(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dbg_5.0.0.6-1_amd64.deb?sv=2015-04-05&sr=b&sig=JYMp4O2vhrEEfd0sDOlCrmQq7HDnI0vTecMDTt184JM%3D&se=2035-04-19T21%3A33%3A35Z&sp=r"
