e90e7fafa56be5df8a188c86b7b417b0cd076530
Bing Wang <bingwang@microsoft.com>
2023-03-24 18:18:02 +0000

    Merged PR 7770531: [202012]Check in vnet-monitor docker
    This PR is to set `INCLUDE_VNET_MONITOR=y` for cisco build.
    The same flag was already enable for `Mellanox` build.

--- slave.mk
+++ slave.mk
@@ -167,6 +167,10 @@ ifeq ($(SONIC_INCLUDE_MUX),y)
 INCLUDE_MUX = y
 endif
 
+ifeq ($(SONIC_INCLUDE_VNET_MONITOR),y)
+INCLUDE_VNET_MONITOR = y
+endif
+
 include $(RULES_PATH)/functions
 
 ifeq ($(SONIC_USE_PDDF_FRAMEWORK),y)
@@ -283,6 +287,7 @@ $(info "INCLUDE_SFLOW"                   : "$(INCLUDE_SFLOW)")
 $(info "INCLUDE_NAT"                     : "$(INCLUDE_NAT)")
 $(info "INCLUDE_KUBERNETES"              : "$(INCLUDE_KUBERNETES)")
 $(info "INCLUDE_MUX"                     : "$(INCLUDE_MUX)")
+$(info "INCLUDE_VNET_MONITOR"            : "$(INCLUDE_VNET_MONITOR)")
 $(info "TELEMETRY_WRITABLE"              : "$(TELEMETRY_WRITABLE)")
 $(info "PDDF_SUPPORT"                    : "$(PDDF_SUPPORT)")
 $(info "MULTIARCH_QEMU_ENVIRON"          : "$(MULTIARCH_QEMU_ENVIRON)")
@@ -966,6 +971,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
 	export sonic_utilities_py3_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_UTILITIES_PY3))"
 	export sonic_host_services_py3_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_HOST_SERVICES_PY3))"
 	export include_mux="$(INCLUDE_MUX)"
+	export include_vnet_monitor="$(INCLUDE_VNET_MONITOR)"
 	$(foreach docker, $($*_DOCKERS),\
 		export docker_image="$(docker)"
 		export docker_image_name="$(basename $(docker))"
